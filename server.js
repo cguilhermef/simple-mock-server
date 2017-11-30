2ed2f31a51fd63558046bc230c0c391d2ef4c895
Ubuntu <ubuntu@ip-172-31-60-17.ec2.internal>
2017-11-30 15:55:45 +0000

    Ajustes no server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 })); // for parsing application/x-www-form-urlencoded
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, Authorization,  X-Requested-With, Content-Type, Accept");
   next();
 });
 
